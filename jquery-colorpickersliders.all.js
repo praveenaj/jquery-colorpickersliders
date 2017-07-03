1c38cf48ea6249904463ac875eefe6b73f9e3102
Praveena Sarathchandra <praveenaj@gmail.com>
2017-07-03 18:43:26 +0530

    Update jquery-colorpickersliders.all.js

--- jquery-colorpickersliders.all.js
+++ jquery-colorpickersliders.all.js
@@ -1894,7 +1894,7 @@ window.tinycolor = tinycolor;
             function _renderHue() {
                 var hsla = $.extend(true, {},color.hsla);
                 if(settings.staticHue){
-                    hsla.s = 0.5;
+                    hsla.s = 1;
                     hsla.l = 0.5;
                 }
                 setGradient(elements.sliders.hue, $.fn.ColorPickerSliders.getScaledGradientStops(hsla, "h", 0, 360, 7));
